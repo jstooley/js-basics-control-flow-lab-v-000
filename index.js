b0e2946a2aa25b50b2b6dbefba81efaec184e194
Justin Tooley <jstooley823@gmail.com>
2018-02-14 00:59:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,12 +23,3 @@ function switchOnCharmFromTip(tip){
       'Bye';
   }
 }
-function switchOnCharmFromTip(tip) {
-  if (tip === 'generous') {
-    return 'Thank you so much.';
-  } else if (tip === 'not as generous') {
-    return 'Thank you.';
-  } else {
-    return 'Bye.';
-  }
-}
