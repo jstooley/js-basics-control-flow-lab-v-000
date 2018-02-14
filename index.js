e147f7e3774a9efd5dcc0c6c3cd741e06f519367
Justin Tooley <jstooley823@gmail.com>
2018-02-14 01:03:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip){
   switch (tip) {
     case 'generous':
-      return 'Thank you so much.';
-      break;
+      return 'Thank you so much.'
+
     case 'not as generous':
-      return 'Thank you.';
-      break;
+      return 'Thank you.'
+
     case default:
-      return 'Bye';
+      return 'Bye'
   }
 }
