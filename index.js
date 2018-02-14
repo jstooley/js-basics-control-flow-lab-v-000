e5dfedfaded77ceee997d5ae8e79c622e43eceb7
Justin Tooley <jstooley823@gmail.com>
2018-02-14 00:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 scuberGreetingForFeet(distance){
   switch (distance){
     case distance =< 400:
-    break;
+      break;
     case distance > 2000:
-    break;
-   case distance > 2500:
-   break;
+      break;
+    case distance > 2500:
+      break;
   }
 
 }
