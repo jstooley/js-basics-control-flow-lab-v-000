cf050bb400a989f1bf85d4ad19f678ac73349dde
Justin Tooley <jstooley823@gmail.com>
2018-02-14 00:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-scuberGreetingForFeet(distance){}
+scuberGreetingForFeet(distance){
+  switch (distance){
+    case distance =< 400:
+    break;
+    case distance > 2000:
+    break;
+   case distance > 2500:
+   break;
+  }
+
+}
 
 ternaryCheckCity(){}
 
