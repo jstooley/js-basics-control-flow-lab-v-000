225a8389f85fc03f341be452fdcd42750befe68a
Justin Tooley <jstooley823@gmail.com>
2018-02-14 00:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ scuberGreetingForFeet(distance){
       'This one is on me!'
       break;
     case distance > 2000:
+      'I will gladly take your thirty bucks.'
       break;
     case distance > 2500:
       break;
