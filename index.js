165660ac2c250000f3914c4eca9084a0457267c4
Justin Tooley <jstooley823@gmail.com>
2018-02-14 00:52:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function scuberGreetingForFeet(distance){
   switch (distance){
     case distance =< 400:
-      'This one is on me!'
+      return 'This one is on me!'
       break;
     case distance > 2000:
-      'I will gladly take your thirty bucks.'
+      return 'I will gladly take your thirty bucks.'
       break;
     case distance > 2500:
-      'No can do.'
+      return 'No can do.'
       break;
   }
 
